78154f9e1f8ea82eaba539185c7151dc0a878f91
NastiaPr3 <Anastasiaprisich151@ukr.net>
2022-06-23 16:52:20 +0300

    hw1 simpsons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Character from "./components/Character";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Character
+      name = {"Bart"}
+      desc = {'Bartholomew "Bart" Jojo Simpson (born April 1[7] or February 23[8]) is the mischievous, rebellious, misunderstood, disruptive and "potentially dangerous" oldest child of the Simpson family in The Simpsons.'}
+      pic = {'https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png'}
+      />
+        <Character
+            name = {"Homer"}
+            desc = {'Homer Jay Simpson (born May 12, 1951)[36] is the main protagonist of The Simpsons series (or show).'}
+            pic = {'https://static.wikia.nocookie.net/simpsons/images/0/02/Homer_Simpson_2006.png'}
+        />
+        <Character
+            name = {"Marge"}
+            desc = {'Marjorie Jacqueline "Marge" Simpson (née Bouvier[11]) is the homemaker and matriarch of the Simpson family. '}
+            pic = {'https://upload.wikimedia.org/wikipedia/ru/0/0b/Marge_Simpson.png'}
+        />
+        <Character
+            name = {"Lisa"}
+            desc = {'Lisa Marie Simpson (born May 9)[9] is the elder daughter and middle child of the Simpson family and one of the two tritagonists (along with Marge,) of The Simpsons series.'}
+            pic = {'https://static.wikia.nocookie.net/simpsons/images/f/fa/185px-Lisa_Simpson2.png'}
+        />
+        <Character
+            name = {"Maggie"}
+            desc = {'Margaret Evelyn[10] Lenny "Maggie" Simpson[11] (born January 12, 1989) is the 1-year old daughter and youngest child of Marge and Homer Simpson, the baby sister to Bart and Lisa Simpson and one of the five main characters of The Simpsons. '}
+            pic = {'https://upload.wikimedia.org/wikipedia/ru/9/9d/Maggie_Simpson.png'}
+        />
     </div>
   );
 }
---
+++ src/components/Character.js
@@ -0,0 +1,11 @@
+function Character(props) {
+let {name, desc, pic} = props;
+
+return (<div>
+    <h2>{name}</h2>
+    <p>{desc}</p>
+    <img src={pic} alt=""/>
+</div>)
+}
+
+export default Character;
